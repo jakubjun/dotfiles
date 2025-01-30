e8e1af42e8be73975aaa248a319742a1e583914d
Jun, Jakub <jakubjun21@gmail.com>
2025-01-30 10:02:52 +0100

    update fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -15,4 +15,14 @@ function nvm_use_on_dir --on-variable PWD
     end
 end
 
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
 alias lazynvim='NVIM_APPNAME="lazynvim" nvim'
+alias n="nvim	"
